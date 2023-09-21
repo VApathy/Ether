7f035172f90c2765feaee7293ca878d5a8f0b4a1
VApathy <96137172+VApathy@users.noreply.github.com>
2023-09-21 10:37:41 -0700

    Powershell Script to remotely install printers through Intune
    Change the printer details to match the printer you would like to install. Before packaging it for intune, put the printer driver into the same directory and adjust the infPath variable to match.

---
+++ Remove Preinstalled Apps.ps1
@@ -0,0 +1,17 @@
+﻿$apps = @(
+    '*xbox.tcui*',
+    '*xboxgameoverlay*',
+    '*xboxgamingoverlay*',
+    '*xboxidentityprovider*',
+    '*xboxspeechtotextoverlay*',
+    'MicrosoftTeams'
+)
+
+foreach ($app in $apps) {
+    $package = Get-AppxPackage -Name $app
+    if ($package) {
+        $package | Remove-AppxPackage
+    } else {
+        Write-Output "App with name $app not found on the system."
+    }
+}
